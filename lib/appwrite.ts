462e585a76c101e1e729e27e63ea044232ab62eb
Emmanuel Ochieng <eochieng16@gmail.com>
2025-11-28 12:47:38 +0300

    Setup Appwrite SDK & Auth functions

---
+++ lib/appwrite.ts
@@ -0,0 +1,81 @@
+import * as linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser';
+import { Account, Avatars, Client, OAuthProvider, } from 'react-native-appwrite';
+
+export const config = {
+    platform: 'com.manucho.restate',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
+
+export const client = new Client()
+
+client.setEndpoint(config.endpoint!).setProject(config.projectId!).setPlatform(config.platform)
+
+export const avatar = new Avatars(client)
+export const account = new Account(client)
+
+export async function login() {
+    try {
+        const redirectUri = linking.createURL('/')
+
+        const response = await account.createOAuth2Token({
+            provider: OAuthProvider.Google, success: redirectUri
+        })
+
+        if (!response) throw new Error('Failed to login')
+
+        const browserResult = await WebBrowser.openAuthSessionAsync(
+            response.toString(),
+            redirectUri
+        )
+
+        if (browserResult.type !== 'success') throw new Error('Failed to login')
+
+        const url = new URL(browserResult.url)
+
+        const secret = url.searchParams.get('secret')?.toString()
+        const userId = url.searchParams.get('userId')?.toString()
+
+        if (!secret || !userId) throw new Error('Failed to login')
+
+        const session = await account.createSession(userId, secret)
+
+        if (!session) throw new Error('Failed to create a session')
+
+        return true
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
+
+export async function logout() {
+    try {
+        await account.deleteSession({
+            sessionId: 'current'
+        })
+        return true
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
+
+export async function getUser() {
+    try {
+        const response = await account.get()
+
+        if (response.$id) {
+            const userAvatar = avatar.getInitials({ name: response.name })
+            return {
+                ...response,
+                avatar: userAvatar.toString()
+            }
+        }
+
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
