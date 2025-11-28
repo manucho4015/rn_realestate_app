12ec79bf198d75ad0a7e40a2760403af41547d2c
Emmanuel Ochieng <eochieng16@gmail.com>
2025-11-28 13:47:21 +0300

    Integrate Authentication

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,18 @@
+import { useGlobalContext } from "@/lib/global-provider";
+import { Slot } from "expo-router";
+import { ActivityIndicator } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AppLayout() {
+    const { loading, isLoggedIn } = useGlobalContext()
+
+    if (loading) {
+        return (
+            <SafeAreaView className="bg-white h-full flex justify-center items-center">
+                <ActivityIndicator className="text-primary-300" size="large" />
+            </SafeAreaView>
+        )
+    }
+
+    return <Slot />
+}
