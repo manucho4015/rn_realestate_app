a631d9d62a1954021d51a13d15a9ac07dc455189
Emmanuel Ochieng <eochieng16@gmail.com>
2025-12-01 11:50:21 +0300

    update home page content

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -19,9 +19,19 @@ export default function Index() {
           </View>
           <Image source={icons.bell} className="size-6" />
         </View>
+
+        <Search />
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
 
-      <Search />
     </SafeAreaView>
   );
 }
